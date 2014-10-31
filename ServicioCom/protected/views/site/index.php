ff6275e43b917f37a8d9fb89b29fa4c0e57a1a5f
yonkishum <yonkishum@gmail.com>
2014-10-31 15:06:02 -0430

    Adicional los Archivos Nuevos
    Integración de los Archivos a GitHub

---
+++ ServicioCom/protected/views/site/index.php
@@ -0,0 +1,21 @@
+<?php
+/* @var $this SiteController */
+
+$this->pageTitle=Yii::app()->name;
+?>
+
+<h1>Bienvenido a la <br><i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+
+<p>Este sistema esta realizado para ayudar al Seminario Nuestra Señora del Socorro, para la administración de su Biblioteca.</p>
+
+<br>
+
+<img src="images/escudo.png" alt="Logo Seminario nuestra Señora del Socorro">
+
+
+<br><br>
+
+<p>Por los momentos solo se trabaja con el usuario Administrador para poder ingresar solo teclea:</p>
+
+<h2>Usuario: admin</h2>
+<h2>Pass: 1234</h2>
